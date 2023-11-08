c110b3586b84ba1a1e6c881025053d009dc9977f
nguyenthequang0802 <quangnguyenthe7@gmail.com>
2023-11-09 00:27:11 +0700

    upload DB of book

--- website/app/Http/Controllers/admin/BookController.php
+++ website/app/Http/Controllers/admin/BookController.php
@@ -4,17 +4,13 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BookController extends Controller
 {
     //
     public function index(){
-        $books = [
-          ['id'=>'1', 'name'=>'Doraemon', 'category'=>'Truyện tranh'],
-          ['id'=>'2', 'name'=>'Shin-cậu bé bút chì', 'category'=>'Truyện tranh'],
-          ['id'=>'3', 'name'=>'Harry porter', 'category'=>'Tiểu thuyết'],
-          ['id'=>'4', 'name'=>'Thát tịch không mưa', 'category'=>'Ngôn tình']
-        ];
+        $books = DB::table('books')->get();
         return view('admin.content.book.index', ['books'=>$books]);
     }
 }
